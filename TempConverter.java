9d18c7c8d8c91c29d91acc7a490ee24ee71d216b
rodney anderson <arodcinco55@gmail.com>
2016-02-18 00:38:59 -0500

    Temperature Converter
    Converts fahrenheit to celsius and vice versa

---
+++ TempConverter.java
@@ -0,0 +1,15 @@
+public class TempConverter {
+	
+	public static void main(String[]args){
+		final int BASE = 32;
+		final double CONVERSION_FACTOR = 9.0/5.0;
+		
+		double fahrenheitTemp;
+		int celsiusTemp = -6; //value to convert 
+		
+		fahrenheitTemp = celsiusTemp * CONVERSION_FACTOR + BASE;
+		
+		System.out.println("Celsius Temperature: " + celsiusTemp);
+		System.out.println("Fahrenheit Temperature: " + fahrenheitTemp);
+	}
+}
